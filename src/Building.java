e3b62224edd9ac80db8c4f09e4d9059bd4f26f61
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 01:17:21 +1000

    add javadoc of building class

--- src/Building.java
+++ src/Building.java
@@ -5,33 +5,56 @@ import org.newdawn.slick.SlickException;
 
 public abstract class Building extends Sprite implements Selectable{
 	private static final String HIGHLIGHT_LARGE = "assets/highlight_large.png";
-	
 	private Image selectImage;
-	private int selectTime = 0;
+	
+	/**
+	 * Building common constructor
+	 * @param x building's x coordinate
+	 * @param y building's y coordinate
+	 * @param imageSrc building's image
+	 * @param camera camera
+	 * @throws SlickException
+	 */
 	public Building(double x, double y, String imageSrc, Camera camera) throws SlickException {
 		super(x, y, imageSrc, camera);
 		
 	}
 	
+	/**
+	 * get Image of building
+	 * @return selectImage
+	 */
 	public Image getImage() {return selectImage;}
 	
+	/**
+	 * if the building is selected 
+	 * @return return true if building is selected 
+	 */
 	@Override
 	public boolean isSelect() {
 		return selectImage != null;
 	} 
 	
-	
-	
-	
+	/**
+	 * deSelect the building
+	 */
 	@Override
 	public void deSelect() throws SlickException {
 		selectImage = null;
 	}
+	
+	/**
+	 * select the building
+	 */
 	@Override
 	public void select() throws SlickException {
 		 selectImage = new Image(HIGHLIGHT_LARGE);
 	}	
 	
+	/**
+	 * render the building and selected image
+	 * @param g Graphics
+	 */
 	@Override
 	public void render(Graphics g) {
 		if(isSelect()) {
