1d8236b18d735a1d085a98d25d00f1d1cc36606e
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:19:45 +1000

    finish commandCenter class javadoc

--- src/CommandCenter.java
+++ src/CommandCenter.java
@@ -2,13 +2,29 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-
+/**
+ * commandCenter class 
+ */
 public class CommandCenter extends Building implements Creatable {
 	private static final String CENTER_PATH = "assets/buildings/command_centre.png";
+	
+	/**
+	 * constructor of commandcenter
+	 * @param x commandCenter x coordinate
+	 * @param y commandCenter y coordinate
+	 * @param camera camera
+	 * @throws SlickException
+	 */
 	public CommandCenter(double x, double y, Camera camera) throws SlickException{
 		super(x, y, CENTER_PATH, camera);
 	}
 	
+	/**
+	 * create Unit depend on user input
+	 * @param input user input value
+	 * @param x unit x coordinate
+	 * @param y unit y coordinate
+	 */
 	@Override
 	public Unit create(String input, double x, double y) throws SlickException {
 		switch(input) {
@@ -19,14 +35,10 @@ public class CommandCenter extends Building implements Creatable {
 		return null;
 	}
 	
-	public boolean isCreate(Input input) {
-		if(isSelect() && input.isKeyPressed(Input.KEY_1)) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	
+	/**
+	 * render the commandCenter
+	 * @param g render the function of commandCenter
+	 */
 	@Override
 	public void render(Graphics g) {
 		super.render(g);
