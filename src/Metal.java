0f4995f0b05aa959302bca9ba80096af81ef9ccd
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:31:43 +1000

    finish metal class javadoc

--- src/Metal.java
+++ src/Metal.java
@@ -1,11 +1,16 @@
-import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Metal extends Resource {
 	private static final String METAL_PATH = "assets/resources/metal_mine.png";
 	public static final int amount = 500;
 	
-	
+	/**
+	 * Metal constructor
+	 * @param x metal x coordinate
+	 * @param y metal y coordinate
+	 * @param camera world camera
+	 * @throws SlickException
+	 */
 	public Metal(double x, double y, Camera camera) throws SlickException{
 		super(x, y, METAL_PATH, amount, camera);
 	}
