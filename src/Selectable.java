b350973d95b6b746a47721a9ef54fb7a45e45965
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:53:20 +1000

    finish selectable class javadoc

--- src/Selectable.java
+++ src/Selectable.java
@@ -3,14 +3,25 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 /**
- * @author ASUS
- *
+ * select the unit or building if mouse left button is down
  */
 public interface Selectable {
 	
+	/**
+	 * select the sprite
+	 * @throws SlickException
+	 */
 	public void select() throws SlickException;
 	
+	/**
+	 * deSelect the sprite in the world 
+	 * @throws SlickException
+	 */
 	public void deSelect() throws SlickException;
 	
+	/**
+	 * if the sprite is selected
+	 * @return if is selected, return true, else return false.
+	 */
 	public boolean isSelect();
 }
