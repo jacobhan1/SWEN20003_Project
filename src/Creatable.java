d5c3e5721dd2a5e161c330df811de18e41b0d1d6
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:22:19 +1000

    finish creatable class javadoc

--- src/Creatable.java
+++ src/Creatable.java
@@ -8,8 +8,14 @@ import org.newdawn.slick.SlickException;
  */
 public interface Creatable {
 	
-	
+	/**
+	 * every creatable sprite could create a new sprite
+	 * @param input input name
+	 * @param x creatable sprite x coordinate
+	 * @param y creatable sprite y coordinate
+	 * @return created new sprite
+	 * @throws SlickException
+	 */
 	public Sprite create(String input, double x, double y) throws SlickException;
 	
-	public void render(Graphics g);
 }
