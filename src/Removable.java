1f201443c1257a99bd93c0ecc3d884e63846f81b
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:40:34 +1000

    finish removable class javadoc

--- src/Removable.java
+++ src/Removable.java
@@ -1,4 +1,10 @@
-
+/**
+ * removable class has the method to remove the sprite from the world.
+ */
 public interface Removable {
+	
+	/**
+	 * remove the sprite from the world
+	 */
 	public void remove();
 }
