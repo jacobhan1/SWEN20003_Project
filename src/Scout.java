25f1b8b0ac2a0d7a0563f64bfbb198967fc1be17
jacob <hanxu.jacobhan@gmail.com>
2019-05-27 23:51:11 +1000

    finish scout class javadoc

--- src/Scout.java
+++ src/Scout.java
@@ -1,10 +1,19 @@
 import org.newdawn.slick.SlickException;
-
+/**
+ * scout is the fastest sprite in the game world
+ */
 public class Scout extends Unit {
 
 	private static final String SCOUT_PATH = "assets/units/scout.png";
 	private static final double SPEED = 0.3;
 	
+	/**
+	 * Scout constructor
+	 * @param x scout x coordinate
+	 * @param y scout y coordinate
+	 * @param camera world camera
+	 * @throws SlickException
+	 */
 	public Scout(double x, double y, Camera camera) throws SlickException {
 		super(x, y, SCOUT_PATH, SPEED, camera);
 	}
